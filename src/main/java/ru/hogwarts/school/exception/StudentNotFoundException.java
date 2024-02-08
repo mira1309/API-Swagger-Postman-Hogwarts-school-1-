fba37322652fb4261dacdd6f6a34730b066e1fd3
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-02-08 17:45:33 +0300

    Введение в базы данных

--- src/main/java/ru/hogwarts/school/exception/StudentNotFoundException.java
+++ src/main/java/ru/hogwarts/school/exception/StudentNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class StudentNotFoundException extends RuntimeException{
     public StudentNotFoundException(String message) {
-        super (message);
+        super  (message);
     }
     public StudentNotFoundException (String message, Throwable cause){
         super (message, cause);
